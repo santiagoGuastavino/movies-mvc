63546d92578f9ff453dc901b08d52af189720274
santiagoGuastavino <san.guasta@gmail.com>
2021-06-22 22:27:15 -0300

    setting up express

--- src/app.js
+++ src/app.js
@@ -1,47 +1,15 @@
-// require express
-// let app
-// let path
-// -------------------------------------------------------------------------- //
-// ROUTES
-// REQUIRE ROUTE
-// let requiredRouter = ('path to router odule')
-// USE ROUTES
-// app.use('/route',requiredRouter);
-// -------------------------------------------------------------------------- //
-// GLOBAL MIDDLEWARES
-// app.use('requiredMiddleware'); //ex: logs
-// -------------------------------------------------------------------------- //
-// SET PUBLIC
-// app.use(express.static('public'))
-// -------------------------------------------------------------------------- //
-// EJS TEMPLATE ENJINE
-// npm i ejs
-// app.set 'view engine' , 'ejs'
-// app.set 'views' , viewPath
-// -------------------------------------------------------------------------- //
-// ERROR
-// app.use((req,res,next) => {
-//  res.status(404).render('not-found-view.ejs', { status: 404 } );
-//  next();
-// })
-// -------------------------------------------------------------------------- //
-// PUT & DELETE w/ METHOD OVERRIDE
-// npm i method-override
-// let methodOverride = require('method-override');
-// app.use(methodOverride('_method'));
-// IN THE VIEW
-// <form action="/route?_method=PUT/DELETE" method="POST"></form>
-// -------------------------------------------------------------------------- //
-// POST
-// to parse the body
-// app.use(express.urlencoded ({extended:false}));
-// app.use(express.json());
-// -------------------------------------------------------------------------- //
-// SESSION
-// npm i express-session
-// let session = require('express-session')
-// GLOBAL MIDDLEWARE:
-// app.use(session ( { secret:'session-secret' } ))
-// -------------------------------------------------------------------------- //
-// SERVER LISTENING
-// app.listen(3001)
+let express = require('express');
+let app = express();
+let path = require('path');
+
+let viewsPath = path.join(__dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', viewsPath)
+
+app.use(express.static('public'));
+
+let mainRouter = require('./routes/main');
+
+app.use('/', mainRouter);
+
+app.listen(3001, () => console.log('Server running: 3001'));
