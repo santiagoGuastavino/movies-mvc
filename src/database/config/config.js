45fe20d8a651e33adaba73358f78883ded539fb8
santiagoGuastavino <san.guasta@gmail.com>
2021-09-13 15:33:39 -0300

    changed production/development db config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "be08f811240863",
-    "password": "eb5a9e14",
-    "database": "heroku_c13477a0d22e9b2",
-    "host": "us-cdbr-east-04.cleardb.com",
+    "username": "root",
+    "password": null,
+    "database": "movies_db",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "be08f811240863",
+    "password": "eb5a9e14",
+    "database": "heroku_c13477a0d22e9b2",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 };
